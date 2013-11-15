6af021d94454c333a8efc98d2594fbd13dc14314
yuanting0505 <yuantingisme@gmail.com>
2013-11-15 16:43:38 +0800

    Finish sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,21 @@ class User < ActiveRecord::Base
     
     has_secure_password
     validates :password, length:{minimum:6}
+
+    before_create :create_remember_token
+
+    def User.new_remember_token
+    	SecureRandom.urlsafe_base64
+    end
+
+    def User.encrypt(token)
+    	# to_s in case of nil
+    	Digest::SHA1.hexdigest(token.to_s)
+    end
+
+    private
+
+    def create_remember_token
+    	self.remember_token=User.encrypt(User.new_remember_token)
+    end
 end
