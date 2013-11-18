10ed6c820d38db3f0dde5de198de59217d602c76
yuanting0505 <yuantingisme@gmail.com>
2013-11-18 15:03:31 +0800

    change fofo to admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+user=User.find(101)
+user.update_attribute(admin:true)
