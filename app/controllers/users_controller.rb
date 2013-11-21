f7e7e286361c2c44d672d5f86d7ff8a3beae5c4f
yuanting0505 <yuantingisme@gmail.com>
2013-11-21 16:57:40 +0800

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
 
   def show
   	@user=User.find(params[:id])
+    @microposts=@user.microposts.paginate(page:params[:page])
   end
 
   def create
@@ -57,16 +58,7 @@ class UsersController < ApplicationController
   end
 
   # before filters
-  def signed_in_user
-
-    # redirect_to signin_url, notice: "Please sign in." unless signed_in?
-    unless signed_in?
-     store_location
-     flash[:notice]="Please sign in."
-     redirect_to signin_url
-    end
-  end
-
+  
   def correct_user
     # taken from the browser url
     @user=User.find(params[:id])
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,13 @@ SampleApp::Application.routes.draw do
   # get "users/new"
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
+  resources :microposts, only: [:create, :destroy]
+
   root to:'static_pages#home',via:'get'
   match '/help', to:'static_pages#help',via:'get'
   match '/about', to:'static_pages#about', via:'get'
   match '/contact',to:'static_pages#contact', via:'get'
+  
 
   match '/signup',to:'users#new', via:'get'
   match '/signin',to:'sessions#new',via:'get'
