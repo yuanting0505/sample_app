bf79d89092fcc4778b21d81b3368ef29f593b447
yuanting0505 <yuantingisme@gmail.com>
2013-11-22 19:06:39 +0800

    middle of chapter 11

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ActiveRecord::Base
     has_many :microposts,dependent: :destroy
-	validates :name, presence: true, length:{ maximum: 50 }
+    has_many :relationships, foreign_key: "follower_id",dependent: :destroy
+    has_many :reverse_relationships, foreign_key: "followed_id",
+                                     class_name:  "Relationship",
+                                     dependent:   :destroy
+    has_many :followed_users, through: :relationships,source: :followed
+    has_many :followers, through: :reverse_relationships, source: :follower    
+    validates :name, presence: true, length:{ maximum: 50 }
 
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
     validates :email, presence: true, format:{ with: VALID_EMAIL_REGEX}	,
@@ -24,6 +30,18 @@ class User < ActiveRecord::Base
     def User.encrypt(token)
     	# to_s in case of nil
     	Digest::SHA1.hexdigest(token.to_s)
+    end
+
+    def following?(other_user)
+        relationships.find_by(followed_id: other_user.id)
+    end
+
+    def follow!(other_user)
+        relationships.create!(followed_id: other_user.id)
+    end
+
+    def unfollow!(other_user)
+        relationships.find_by(followed_id:other_user.id).destroy
     end
 
     private
